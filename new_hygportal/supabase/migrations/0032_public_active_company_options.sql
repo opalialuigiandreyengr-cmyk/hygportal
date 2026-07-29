1409c68a3044024e8f31acf5b335d827a2d52b16
John Denver Candasua <johndenvercandasua02252004@gmail.com>
2026-07-29 11:41:14 +0800

    Revert "Merge branch 'main' of https://github.com/opalialuigiandreyengr-cmyk/hygportal"
    This reverts commit ab62f5925191ec96e545c1b9726a71bd6fd5c5c3, reversing
    changes made to c395c0549bfa62c1d986d260165cc1a8828257ab.

--- new_hygportal/supabase/migrations/0032_public_active_company_options.sql
+++
@@ -1,8 +0,0 @@
-drop policy if exists "Anyone can read active companies" on public.companies;
-
-create policy "Anyone can read active companies"
-on public.companies for select
-to anon, authenticated
-using (is_active = true);
-
-notify pgrst, 'reload schema';
